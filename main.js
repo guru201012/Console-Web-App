4df9a41497f42ab34ca4f4f905a7910937bde90a
guru201012 <88066416+guru201012@users.noreply.github.com>
2021-12-11 17:21:48 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("My First Console");
+var name1="GURU";
+var name2="Preetham";
+var name=name1.concat(name2);
+console.log(name);
+var a=10;
+var b=5;
+var add=a+b;
+console.log(add);
+var sub=a-b;
+console.log(sub);
